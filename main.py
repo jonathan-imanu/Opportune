ba4b02c6f2bab5affb7a863e0d32b7c5f46997de
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-08-30 23:55:12 -0400

    feat: added search jobs

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from linkedin_api import Linkedin
 from dotenv import load_dotenv
 
 
-# Authenticate using any Linkedin account credentials
 load_dotenv()
 
 linkedin_username = os.getenv("LINKEDIN_USERNAME")
 linkedin_password = os.getenv("LINKEDIN_PASSWORD")
 
-scraper = Linkedin(linkedin_username, linkedin_password)
+linkedin_scraper = Linkedin(linkedin_username, linkedin_password)
 
+# Get latest software jobs in Ontario 
+
+jobs = linkedin_scraper.search_jobs(keywords="Software",
+                     location_name="Ontario, Canada",
+                     experience=["1"],
+                     remote=["1", "2", "3"],
+                     listed_at=720000)
+                     
