03ed02f4a1e2ba1936b2e0351701d98fd5b186f8
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-08-31 20:07:04 -0400

    feat: added gspread for writing to sheet

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Opportune
-A simple script that fetches the latest jobs from LinkedIn and similar sites 
+
+A simple script that fetches the latest jobs from LinkedIn and similar sites and uploads them to a Google Sheet in this format.
+
+## Prequisites
+
+The following is expected:
+
+A .env file with the following contents:
+
+```bash
+LINKEDIN_USERNAME=""
+LINKEDIN_PASSWORD=""
+```
+
+A valid credentials.json()
+
+## Getting Started
+
+```bash
+python -m venv venv
+pip install -r requirements.txt
+python -u "/home/opportune/main.py"
+```
--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import os
 from linkedin_api import Linkedin
 from dotenv import load_dotenv
+import pandas as pd
+import gspread
 
 
 load_dotenv()
 
 linkedin_username = os.getenv("LINKEDIN_USERNAME")
 linkedin_password = os.getenv("LINKEDIN_PASSWORD")
+service_path = os.getenv("SERVICE_PATH")
+tracker_sheet = os.getenv("SHEET_NAME")
 
 linkedin_scraper = Linkedin(linkedin_username, linkedin_password)
 
 # Get latest software jobs in Ontario 
-
 jobs = linkedin_scraper.search_jobs(keywords="Software",
                      location_name="Ontario, Canada",
                      experience=["1"],
                      remote=["1", "2", "3"],
                      listed_at=720000)
-                     
+
+
+gc = gspread.service_account(filename=service_path)
+sh = gc.open(tracker_sheet)
+
+print(sh.sheet1.get('A1'))
+
